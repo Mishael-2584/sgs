bb82052e763a2bfcd04bff9f2f2106908d6b05c9
SS-Johnny <johnnythrust0@gmail.com>
2022-08-21 21:06:10 +0100

    change

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,14 +52,14 @@
                <a href="{{route('lecturer-import')}}"><i class="fa fa-user"></i> <span class="nav-label">Upload Students</span> </a>
            </li>
            <li class="#">
-               <a href="#"><i class="fa fa-users"></i> <span class="nav-label">Add Students</span></a>
+               <a href="{{route('lecturer-add-material')}}"><i class="fa fa-users"></i> <span class="nav-label">Add Students</span></a>
            </li>
            
            <li class="#">
                <a href="#"><i class="fa fa-eye"></i> <span class="nav-label">All Results</span></a>
            </li>
            <li class="">
-               <a href=""><i class="fa fa-users"></i> <span class="nav-label">User management</span> </a>
+                <a href="{{route('lecturer-allresults')}}"><i class="fa fa-users"></i> <span class="nav-label">User management</span> </a>
            </li>
            @endif --
             
