65a98092a02a7b466e503ef86c2e8b7caef27bb7
SS-Johnny <johnnythrust0@gmail.com>
2022-08-22 20:13:03 +0100

    Changes

--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -8,29 +8,50 @@
          }
          .white-text{
             color:#ffffff;
+            font-weight: 800;
          }
          .gold-text{
             color:#b98d3d;
          }
             .gold-text-login{
             color:#b98d3d;
-            font-size:18px;
+            font-size:28px;
+            font-weight: 700;
          }
          .btn-white {
              color:#004282;
              background: #ffffff;
              font-weight: bold; 
+             border-radius: 20px;
          }
          .btn-white:hover{
             background-color: #b98d3d;
             color:#ffffff;
+            transition: ease-in 1.1s;
          }
          .forgot-password, a:link{
              color: #b98d3d;
              font-size: 16px;
          }
      .forgot-password:hover{
-         color: #b98d3d
+         color: white;
+         background-color: #b98d3d;
+         transition: ease-in 1s;
+     }
+     .forgot-password small{
+        border: solid 1px #b98d3d;     
+        padding: .1rem 1rem;
+     }
+     .form-control{
+        border-radius: 20px;
+        /* margin-left: 10px; */
+     }
+     .form-group input{
+        position: relative;
+        padding-left: 2rem;
+     }
+     .logg{
+        font-size: 15px;
      }
      </style>
 
@@ -42,10 +63,10 @@
                   <h1 class="logo-name"><img src="{{asset('BU_logo.png')}}" alt="" style="width: 300px;"></h1>
 
             </div>
-            <h3 class="white-text">SEMESTER GRADING SYSTEM</h3>
+            <h2 class="white-text">SEMESTER GRADING SYSTEM</h2>
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <p class="gold-text-login">Login</p>
+            <h2 class="gold-text-login">LOGIN</h2>
             <form class="m-t" method="POST" action="{{route('auth')}}">
                  @csrf
                 <div class="form-group">
@@ -54,7 +75,7 @@
                 <div class="form-group">
                     <input type="password" name="password" class="form-control" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-white block full-width m-b">Login</button>
+                <button type="submit" class="btn btn-white block full-width m-b logg">LOGIN</button>
                 {{-- <a class="btn btn-sm btn-white btn-block" href="login.html">Login</a> --}}
 
                 <a class="forgot-password" href="#"><small>Forgot password?</small></a>
--- resources/views/auths/signup.blade.php
+++ resources/views/auths/signup.blade.php
@@ -8,29 +8,45 @@
          }
          .white-text{
             color:#ffffff;
+            font-weight: 800;
          }
          .gold-text{
             color:#b98d3d;
          }
             .gold-text-login{
             color:#b98d3d;
-            font-size:18px;
+            font-size:28px;
+            font-weight: 700;
          }
          .btn-white {
              color:#004282;
              background: #ffffff;
              font-weight: bold; 
+             border-radius: 20px;
          }
          .btn-white:hover{
             background-color: #b98d3d;
             color:#ffffff;
+            transition: ease-in 1.1s;
          }
          .forgot-password, a:link{
              color: #b98d3d;
              font-size: 16px;
          }
-     .forgot-password:hover{
-         color: #b98d3d
+        .forgot-password:hover{
+            color: #b98d3d;
+            transition: ease-in 1s;
+        }
+        .form-control{
+            border-radius: 20px;
+            /* margin-left: 10px; */
+        }
+        .form-group input{
+            position: relative;
+            padding-left: 2rem;
+        }
+        .logg{
+        font-size: 15px;
      }
      </style>
 
@@ -42,10 +58,10 @@
                   <h1 class="logo-name"><img src="{{asset('BU_logo.png')}}" alt="" style="width: 300px;"></h1>
 
             </div>
-            <h3 class="white-text">SEMESTER GRADING SYSTEM</h3>
+            <h2 class="white-text">SEMESTER GRADING SYSTEM</h2>
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <p class="gold-text-login">Sign Up</p>
+            <p class="gold-text-login">SIGN UP</p>
             <form class="m-t" method="POST" action="{{route('register')}}">
                  @csrf
                  <div class="form-group">
@@ -57,11 +73,11 @@
                 <div class="form-group">
                     <input type="password" name="password" class="form-control" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-white block full-width m-b">Sign Up</button>
+                <button type="submit" class="btn btn-white block full-width m-b logg">SIGN UP</button>
                 {{-- <a class="btn btn-sm btn-white btn-block" href="login.html">Login</a> --}}
 
                 <p class="text-center white-text"><small>Do you want to Login instead?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="{{route('login')}}">Login</a>
+                <a class="btn btn-sm btn-white btn-block m-b" href="{{route('login')}}">LOGIN</a>
             </form>
             {{-- <p class="m-t white-text"> <small>SGS &copy; <?php echo date("Y"); ?></small> </p> --}}
         </div>
