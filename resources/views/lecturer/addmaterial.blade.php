917b3a512bf9faa552f1fda54d989a38a26305f6
SS-Johnny <johnnythrust0@gmail.com>
2022-08-23 22:04:32 +0100

    aa

--- resources/views/lecturer/addmaterial.blade.php
+++ resources/views/lecturer/addmaterial.blade.php
@@ -11,7 +11,7 @@
                         <li class="breadcrumb-item">
                             <a href="{{route('lecturer-dashboard')}}">Lecturer</a>
                         </li>
-                        <li class="breadcrumb-item active">
+                        <li class="breadcrumb-item">
                             <strong>View Students Results</strong>
                         </li>
                     </ol>
@@ -24,13 +24,38 @@
                         <div class="row">
                             <div class="col-lg-12">
                                <ul class="nav nav-tabs">
-                                <li class="text-color"><a class="nav-link active" data-toggle="tab" href="#tab-1"><i class="fa fa-plus"></i>ADD NEW QUIZ</a></li>
-                                <li class="text-color"><a class="nav-link active" data-toggle="tab" href="#tab-2"><i class="fa fa-file"></i>VIEW CURRENT QUIZZES</a></li>
+                                <li class="text-color"><a class="nav-link" data-toggle="tab" href="#tab-1"><i class="fa fa-plus"></i>ADD NEW QUIZ</a></li>
+                                <li class="text-color"><a class="nav-link" data-toggle="tab" href="#tab-2"><i class="fa fa-file"></i>VIEW CURRENT QUIZZES</a></li>
                                 <li><a class="text-color" data-toggle="tab" href="#tab-3"><i class="fa fa-plus"></i>ADD NEW ASSIGNMENTS</a></li>
-                                <li class="text-color"><a class="nav-link active" data-toggle="tab" href="#tab-4"><i class="fa fa-file "></i>VIEW CURRENT ASSIGNMENTS</a></li>
+                                <li class="text-color"><a class="nav-link " data-toggle="tab" href="#tab-4"><i class="fa fa-file "></i>VIEW CURRENT ASSIGNMENTS</a></li>
+                               <ul class="nav nav-tabs" id="onclik">
+                                <li class="text-color clickd"><a class="nav-link cliks" data-toggle="tab" href="#tab-1"><i class="fa fa-plus"></i>ADD NEW QUIZ</a></li>
+                                {{-- <li class="ssd"><a class="nav-link text-color clikk" data-toggle="tab" href="#tab-2"><i class="fa fa-address-book"></i>ADD NEW ASSIGNMENTS</a></li> --}}
                                </ul>
+                               {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.css" /> --}}
+                               <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+                                <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.js"></script>
+                               <script>
+                                $(document).ready(function(){
+                                    var ssd = $("a.active")
+                                    $(".clik").click(function(){
+                                        if (!ssd.hasClass("active")){
+                                            $(".clikk").addClass("a.active")
+                                        }else{
+                                            $(".cliks").addClass("active")
+                                        }
+                                    })})
+                                    // $(".clikk").click(function(){
+                                    //     if($(".clikd") hasClass("active")){
+                                    //         $(".clikk").removeClass("active")
+                                    //         $(".cliks").addClass("active")
+                                    //     }else{
+                                    //         $(".cliks").addClass("active")
+                                    //     }
+                                    // })
+                               </script>
                            <div class="tab-content">
-							  <div id="tab-1" class="tab-pane active">
+							  <div id="tab-1" class="tab-pane">
                             <div class="ibox-content">
                                 <form method="POST" action="{{route('lecturer-add-quiz')}}">
                                     @csrf
@@ -59,7 +84,7 @@
                             <div id="tab-2" class="tab-pane">
                                 <div class="ibox">
                                     <div class="ibox-content forum-container">
-                                        <div class="forum-item active">
+                                        <div class="forum-item">
                                             @isset($quiztypes)
                                             @foreach ($quiztypes as $quiz_type)
                                             <div class="row">
@@ -120,7 +145,7 @@
                               <div id="tab-4" class="tab-pane">
                                 <div class="ibox">
                                     <div class="ibox-content forum-container">
-                                        <div class="forum-item active">
+                                        <div class="forum-item">
                                             @isset($assignmenttypes)
                                             @foreach ($assignmenttypes as $ass_type)
                                             <div class="row">
